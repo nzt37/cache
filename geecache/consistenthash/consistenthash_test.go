8946113887aeac9c37203b45c6c6fa003315741c
yzang <40187181@qq.com>
2022-06-06 16:43:34 +0800

    分布式cache

---
+++ geecache/consistenthash/consistenthash_test.go
@@ -0,0 +1,46 @@
+package consistenthash
+
+import (
+	"strconv"
+	"testing"
+	"fmt"
+)
+
+func TestHashing(t *testing.T) {
+	hash := New(3, func(key []byte) uint32 {
+		i, _ := strconv.Atoi(string(key))
+		return uint32(i)
+	})
+
+	// Given the above hash function, this will give replicas with "hashes":
+	// 2, 4, 6, 12, 14, 16, 22, 24, 26
+	hash.Add("2", "4", "6")
+
+	testCases := map[string]string{
+		"2":  "2",
+		"6": "6",
+		"23": "4",
+		"27": "2",
+	}
+	for i := 0; i < 100; i++ {
+		fmt.Println(i,hash.Get(strconv.Itoa(i)))
+	}
+	for k, v := range testCases {
+		if hash.Get(k) != v {
+			t.Errorf("Asking for %s, should have yielded %s", k, v)
+		}
+	}
+
+	// Adds 8, 18, 28
+	hash.Add("8")
+
+	// 27 should now map to 8.
+	testCases["27"] = "8"
+
+	for k, v := range testCases {
+		if hash.Get(k) != v {
+			t.Errorf("Asking for %s, should have yielded %s", k, v)
+		}
+	}
+
+}
